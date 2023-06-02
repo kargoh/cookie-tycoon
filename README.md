a476c78fd6de4a54a0372cb5997f9d4c6077fa07
doppl3r <deben3@gmail.com>
2023-06-02 08:06:18 -0700

    Add initial project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Background Visualizer
+Preview background images with different options to choose from.
+
+## Local Development
+
+ - Install NodeJS package libraries: `npm install`
+ - Run development libraries `npm run dev`
+ - Use the link it provides
+
+## Deployment
+
+- Run build with `npm run build` to create a fresh `/dist` folder
+
+## Update NPM libraries
+
+- Run `npm outdated`
+- Run `npm i package-name@latest` (replace "package-name" for each package listed)
---
+++ src/vue/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+	import '../scss/style.scss';
+	import { ref } from 'vue';
+
+	// Initialize modes
+	var screen = ref('desktop'); // Options: desktop, tablet, mobile
+	var height = ref('fold'); // Options: fold, full, scale
+</script>
+
+<template>
+
+	<div class="view">
+		<div class="options screen"></div>
+		<div class="device">
+			<div class="image"></div>
+			<div class="content">The Fold</div>
+		</div>
+		<div class="options height">
+			<button>
+				<span class="material-symbols-outlined">fit_screen</span>
+			</button>
+		</div>
+	</div>
+</template>
