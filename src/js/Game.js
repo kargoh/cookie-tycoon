e9720f5168bc300f07560daf9c984cde91e12ee8
Jacob DeBenedetto <jacobdebenedetto@ebilvsmits-4.ur.com>
2024-08-27 16:09:37 -0500

    Add backup & restore functions

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,170 +1,205 @@
 import shop from '../json/shop.json';
 
 class Game {
-    constructor() {
-        // Initialize variables as float types
-        this.cookies = parseFloat(localStorage.getItem('cookies') || 0);
-        this.orders = JSON.parse(localStorage.getItem('orders') || '{}'); // Default empty object
-        this.shop = shop; // Set json object reference
-        this.rate = 0; // Auto increment value
-        this.checkURL();
-    }
+  constructor() {
+    // Initialize variables as float types
+    this.cookies = parseFloat(localStorage.getItem('cookies') || 0);
+    this.orders = JSON.parse(localStorage.getItem('orders') || '{}'); // Default empty object
+    this.shop = shop; // Set json object reference
+    this.rate = 0; // Auto increment value
+    this.checkURL();
+  }
 
-    increment() {
-        // Define singular modifiers
-        var doubleClick = this.getItemAmount('better-click') * (this.getItemAmount('prestige') + 1);
-        var amount = (this.getItemAmount('prestige') + 1) + doubleClick;
+  increment() {
+    // Define singular modifiers
+    var doubleClick = this.getItemAmount('better-click') * (this.getItemAmount('prestige') + 1);
+    var amount = (this.getItemAmount('prestige') + 1) + doubleClick;
 
-        // Update cookies and save
-        this.cookies = this.cookies + amount;
+    // Update cookies and save
+    this.cookies = this.cookies + amount;
+    localStorage.setItem('cookies', this.cookies);
+
+    // Return value for notifications
+    return amount;
+  }
+
+  autoIncrement() {
+    // Define automatic modifiers
+    var oven = this.getItemAmount('oven');
+    var bakery = this.getItemAmount('bakery');
+    var delivery = this.getItemAmount('delivery');
+    var shipment = this.getItemAmount('shipment');
+    var drone = this.getItemAmount('drones');
+    var spaceShip = this.getItemAmount('spaceship');
+    var moonCookies = this.getItemAmount('moon-cookies');
+    var spaceshipEnterprise = this.getItemAmount('spaceship-enterprise');
+    var galacticFactory = this.getItemAmount('galactic-factory');
+    var amount = oven
+    + bakery
+    + delivery
+    + shipment
+    + drone
+    + spaceShip
+    + moonCookies
+    + spaceshipEnterprise
+    + galacticFactory;
+
+    // Update cookies and save
+    this.cookies = this.cookies + amount;
+    this.rate = amount; // Update for Vue label
+    localStorage.setItem('cookies', this.cookies);
+
+    // Return value for notifications
+    return amount;
+  }
+
+  getCookies() {
+    return this.cookies;
+  }
+
+  isPrestigeAvailable() {
+    var _this = this;
+    var isPrestigable = true;
+    var prestigeScale = this.getPrestigeScale();
+
+    // Loop through all shop keys to see if orders are greater than prestige scale
+    Object.keys(this.shop).forEach(function(key) {
+      if (key != "prestige") {
+        var orders = _this.orders[key] || 0;
+        if (orders < prestigeScale){
+          isPrestigable = false;
+        }
+      }
+    })
+
+    return isPrestigable;
+  }
+
+  getPrestigeScale() {
+    return this.shop["prestige"].scale;
+  }
+
+  getItemAmount(key, index = 0) {
+    // Note: "prestige" amount does not scale by "cookie-production"
+    var orders = (this.orders[key] || index);
+    var itemAmount = (key != "prestige") ? (parseInt(orders) * this.shop[key]['cookie-production']) : orders;
+    return itemAmount;
+  }
+
+  purchase(key, $event, notify = function(){}, shopAmount) {
+    var item = this.shop[key];
+    var amount = shopAmount;
+    var unlimited = item.stock == -1; // Boolean
+    var price = this.getPrice(key, amount);
+    var stock = (this.orders[key] || 0);
+
+    if (key == 'prestige') {
+      if (this.isPrestigeAvailable() == true) {
+        this.cookies = 0;
+        this.orders = { prestige: (this.orders['prestige'] || 0) + 1 }
+
         localStorage.setItem('cookies', this.cookies);
-
-        // Return value for notifications
-        return amount;
+        localStorage.setItem('orders', JSON.stringify(this.orders));
+      }
+      else {
+        notify('Must own ' + this.getPrestigeScale() + ' of all shop items.', $event);
+      }
     }
-
-    autoIncrement() {
-        // Define automatic modifiers
-        var oven = this.getItemAmount('oven');
-        var bakery = this.getItemAmount('bakery');
-        var delivery = this.getItemAmount('delivery');
-        var shipment = this.getItemAmount('shipment');
-        var drone = this.getItemAmount('drones');
-        var spaceShip = this.getItemAmount('spaceship');
-        var moonCookies = this.getItemAmount('moon-cookies');
-        var spaceshipEnterprise = this.getItemAmount('spaceship-enterprise');
-        var galacticFactory = this.getItemAmount('galactic-factory');
-        var amount = oven
-        + bakery
-        + delivery
-        + shipment
-        + drone
-        + spaceShip
-        + moonCookies
-        + spaceshipEnterprise
-        + galacticFactory;
-
-        // Update cookies and save
-        this.cookies = this.cookies + amount;
-        this.rate = amount; // Update for Vue label
-        localStorage.setItem('cookies', this.cookies);
-
-        // Return value for notifications
-        return amount;
-    }
-
-    getCookies() {
-        return this.cookies;
-    }
-
-    isPrestigeAvailable() {
-        var _this = this;
-        var isPrestigable = true;
-        var prestigeScale = this.getPrestigeScale();
-
-        // Loop through all shop keys to see if orders are greater than prestige scale
-        Object.keys(this.shop).forEach(function(key) {
-            if (key != "prestige") {
-                var orders = _this.orders[key] || 0;
-                if (orders < prestigeScale){
-                    isPrestigable = false;
-                }
-            }
-        })
-
-        return isPrestigable;
-    }
-
-    getPrestigeScale() {
-        return this.shop["prestige"].scale;
-    }
-
-    getItemAmount(key, index = 0) {
-        // Note: "prestige" amount does not scale by "cookie-production"
-        var orders = (this.orders[key] || index);
-        var itemAmount = (key != "prestige") ? (parseInt(orders) * this.shop[key]['cookie-production']) : orders;
-        return itemAmount;
-    }
-
-    purchase(key, $event, notify = function(){}, shopAmount) {
-        var item = this.shop[key];
-        var amount = shopAmount;
-        var unlimited = item.stock == -1; // Boolean
-        var price = this.getPrice(key, amount);
-        var stock = (this.orders[key] || 0);
-
-        if (key == 'prestige') {
-            if (this.isPrestigeAvailable() == true) {
-                this.cookies = 0;
-                this.orders = { prestige: (this.orders['prestige'] || 0) + 1 }
-
-                localStorage.setItem('cookies', this.cookies);
-                localStorage.setItem('orders', JSON.stringify(this.orders));
-            }
-            else {
-                notify('Must own ' + this.getPrestigeScale() + ' of all shop items.', $event);
-            }
+    else {
+      // Check if there are enough cookies
+      if (this.cookies >= price) {
+        // Purchase if the shop has inventory (or has unlimited "-1" stock)
+        if (stock < item.stock || unlimited == true) {
+          // Top-off amount size (ex: if stock = 18, and shop inventory = 20, then amount = 2)
+          if (stock + amount > item.stock && unlimited == false) {
+            amount = item.stock - stock;
+          }
+  
+          // Update cookies and orders
+          this.cookies -= price; // Subtract price from total
+          this.orders[key] = stock + amount; // Increment order stock
+  
+          // Store cookies/orders in localStorage
+          localStorage.setItem('cookies', this.cookies);
+          localStorage.setItem('orders', JSON.stringify(this.orders));
+          notify('+' + amount.toLocaleString() + ' purchased', $event);
         }
         else {
-            // Check if there are enough cookies
-            if (this.cookies >= price) {
-                // Purchase if the shop has inventory (or has unlimited "-1" stock)
-                if (stock < item.stock || unlimited == true) {
-                    // Top-off amount size (ex: if stock = 18, and shop inventory = 20, then amount = 2)
-                    if (stock + amount > item.stock && unlimited == false) {
-                        amount = item.stock - stock;
-                    }
-    
-                    // Update cookies and orders
-                    this.cookies -= price; // Subtract price from total
-                    this.orders[key] = stock + amount; // Increment order stock
-    
-                    // Store cookies/orders in localStorage
-                    localStorage.setItem('cookies', this.cookies);
-                    localStorage.setItem('orders', JSON.stringify(this.orders));
-                    notify('+' + amount.toLocaleString() + ' purchased', $event);
-                }
-                else {
-                    notify('Sold out!', $event);
-                }
-            }
-            else {
-                notify('Insufficient funds', $event);
-            }
+          notify('Sold out!', $event);
         }
+      }
+      else {
+        notify('Insufficient funds', $event);
+      }
+    }
+  }
+
+  getPrice(key, amount = 1) {
+    var item = this.shop[key];
+    var price = item.price;
+    var addPrice = 0;
+    var stock = (this.orders[key] || 0);
+
+    // Add scale if you have stock in this item
+    if (stock != 0 || amount > 1) {
+      for(let i = 0; i < (amount); i++) {
+        if (i == 0) {
+          price = Math.ceil(Math.pow(item.scale, stock + i) * item.price);
+        } else {
+          addPrice = Math.ceil(Math.pow(item.scale, stock + i) * item.price);
+          price = price + addPrice
+        }
+      }
     }
 
-    getPrice(key, amount = 1) {
-        var item = this.shop[key];
-        var price = item.price;
-        var addPrice = 0;
-        var stock = (this.orders[key] || 0);
+    // Return price with scale
+    return price;
+  }
 
-        // Add scale if you have stock in this item
-        if (stock != 0 || amount > 1) {
-            for(let i = 0; i < (amount); i++) {
-                if (i == 0) {
-                    price = Math.ceil(Math.pow(item.scale, stock + i) * item.price);
-                } else {
-                    addPrice = Math.ceil(Math.pow(item.scale, stock + i) * item.price);
-                    price = price + addPrice
-                }
-            }
-        }
+  checkURL() {
+    var url = location.href;
 
-        // Return price with scale
-        return price;
+    // Clear localStorage if reset parameter exists
+    if (url.includes('reset=true')) {
+      localStorage.clear();
+      location.href = location.protocol + '//' + location.host + location.pathname; // Redirect
     }
+  }
 
-    checkURL() {
-        var url = location.href;
+  backupToChrome() {
+    var data = {
+      cookies: this.cookies,
+      orders: JSON.stringify(this.orders)
+    };
 
-        // Clear localStorage if reset parameter exists
-        if (url.includes('reset=true')) {
-            localStorage.clear();
-            location.href = location.protocol + '//' + location.host + location.pathname; // Redirect
-        }
-    }
+    // Save data object to Chrome storage and 
+    chrome.storage.sync.set(data, function() {
+      dispatchEvent(new CustomEvent('backupToChrome'), { detail: data });
+    });
+  }
+
+  restoreFromChrome() {
+    var keys = ['orders', 'cookies'];
+
+    // Get specific data from Chrome storage
+    chrome.storage.sync.get(keys, function(response) {
+      var data = {
+        cookies: parseFloat(response['cookies'] || 0),
+        orders: JSON.parse(response['orders'] || '{}')
+      };
+      
+      // Update variables from Chrome storage
+      this.cookies = data['cookies'];
+      this.orders = data['orders']; // Default empty object
+
+      // Save to local storage from Chrome storage
+      localStorage.setItem('cookies', data['cookies']);
+      localStorage.setItem('orders', JSON.stringify(data['orders']));
+
+      // Send event to refresh the Vue UI
+      dispatchEvent(new CustomEvent('restoreFromChrome', { detail: data }));
+    }.bind(this));
+  }
 }
 
 export { Game }
--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,90 +1,110 @@
 <script setup>
-	import '../scss/style.scss';
-	import { ref, onMounted, watch } from 'vue';
-    import { Game } from '../js/Game.js';
-    import Cookie from './Cookie.vue';
-    import Background from './Background.vue';
-    import Notifications from './Notifications.vue';
-    import Shop from './Shop.vue';
-	import Review from './Review.vue';
-	import Menu from './Menu.vue';
+  import '../scss/style.scss';
+  import { ref, onMounted, onUnmounted, watch } from 'vue';
+  import { Game } from '../js/Game.js';
+  import Cookie from './Cookie.vue';
+  import Background from './Background.vue';
+  import Notifications from './Notifications.vue';
+  import Shop from './Shop.vue';
+  import Review from './Review.vue';
+  import Menu from './Menu.vue';
 
-	// Initialize Vue variables
-	var ui = ref();
-    var game = ref(window.game = new Game());
-    var background = ref();
-    var notifications = ref();
+  // Initialize Vue variables
+  var ui = ref();
+  var game = ref(window.game = new Game());
+  var background = ref();
+  var notifications = ref();
+  var keyRefresh = ref(0);
 
-    // Increment cookie count (this gets called by Cookies.vue)
-	function incrementCookie() {
-		var amount = game.value.increment()
-		background.value.addCookie();
-		return amount;
-	}
+  // Increment cookie count (this gets called by Cookies.vue)
+  function incrementCookie() {
+    var amount = game.value.increment()
+    background.value.addCookie();
+    return amount;
+  }
 
-	function autoIncrement() {
-		// Increment cookies for worker types (ex: oven, bakery, etc.)
-		setInterval(() => {
-			var amount = game.value.autoIncrement();
-			if (amount > 0) {
-				background.value.addCookie();
-				notify('+' + amount.toLocaleString());
-			}
-		}, 1000);
-	}
+  function autoIncrement() {
+    // Increment cookies for worker types (ex: oven, bakery, etc.)
+    setInterval(() => {
+      var amount = game.value.autoIncrement();
+      if (amount > 0) {
+        background.value.addCookie();
+        notify('+' + amount.toLocaleString());
+      }
+    }, 1000);
+  }
 
-	function notify(text, $event) {
-		notifications.value.addNotification(text, $event);
-	}
+  function notify(text, $event) {
+    notifications.value.addNotification(text, $event);
+  }
 
-	function purchaseAmountToggle() {
-		window.addEventListener("DOMContentLoaded", (event) => {
-			var menuToggles = document.querySelectorAll('.shop-button');
-			var prevToggle;
-			for (var i = 0, len = menuToggles.length; i < len; i++) {
-				menuToggles[i].setAttribute("dataIndex", i)
-				menuToggles[i].addEventListener("click", function(e) {
-					if (prevToggle) {
-						prevToggle.classList.remove("toggle")
-					}
-					menuToggles[e.target.attributes.dataIndex.value].classList.add("toggle")
+  function purchaseAmountToggle() {
+    window.addEventListener("DOMContentLoaded", (event) => {
+      var menuToggles = document.querySelectorAll('.shop-button');
+      var prevToggle;
+      for (var i = 0, len = menuToggles.length; i < len; i++) {
+        menuToggles[i].setAttribute("dataIndex", i)
+        menuToggles[i].addEventListener("click", function(e) {
+          if (prevToggle) {
+            prevToggle.classList.remove("toggle")
+          }
+          menuToggles[e.target.attributes.dataIndex.value].classList.add("toggle")
 
-					prevToggle = menuToggles[e.target.attributes.dataIndex.value]
-				});
-			}
-		});
-	}
+          prevToggle = menuToggles[e.target.attributes.dataIndex.value]
+        });
+      }
+    });
+  }
 
-	watch(ui, function(target) {
-		// Check if UI element is 640x360 (min width/height defined in style.scss)
-		if (target.offsetWidth == 640 && target.offsetHeight == 360) {
-			var popup = document.getElementById('app');
-			popup.style.width = '640px';
-			popup.style.height = '360px';
-		}
-	})
+  function addEventListeners() {
+    addEventListener('restoreFromChrome', refreshUI);
+  }
 
-	onMounted(() => {
-		autoIncrement();
-		purchaseAmountToggle();
-	})
+  function removeEventListeners() {
+    removeEventListener('restoreFromChrome', refreshUI);
+  }
+
+  function refreshUI(e) {
+    keyRefresh.value++; // This forces the UI to refresh
+  }
+
+  watch(ui, function(target) {
+    if (target) {
+      // Check if UI element is 640x360 (min width/height defined in style.scss)
+      if (target.offsetWidth == 640 && target.offsetHeight == 360) {
+        var popup = document.getElementById('app');
+        popup.style.width = '640px';
+        popup.style.height = '360px';
+      }
+    }
+  })
+
+  onMounted(() => {
+    autoIncrement();
+    purchaseAmountToggle();
+    addEventListeners();
+  });
+
+  onUnmounted(() => {
+    removeEventListeners();
+  });
 </script>
 
 <template>
-	<div ref="ui" class="ui">
-		<div class="background">
-			<Background ref="background" />
-		</div>
-		<div class="content">
-			<Cookie :cookies="game.getCookies()" :rate="game.rate" :increment-cookie="incrementCookie" :notify="notify" />
-		</div>
-		<div class="aside">
-			<Shop :game="game" :notify="notify" />
-		</div>
-		<div class="notifications">
-			<Notifications ref="notifications" />
-		</div>
-		<Menu :game="game" />
-		<Review />
-	</div>
+  <div ref="ui" class="ui" :key="keyRefresh">
+    <div class="background">
+      <Background ref="background" />
+    </div>
+    <div class="content">
+      <Cookie :cookies="game.getCookies()" :rate="game.rate" :increment-cookie="incrementCookie" :notify="notify" />
+    </div>
+    <div class="aside">
+      <Shop :game="game" :notify="notify" />
+    </div>
+    <div class="notifications">
+      <Notifications ref="notifications" />
+    </div>
+    <Menu :game="game" />
+    <Review />
+  </div>
 </template>
