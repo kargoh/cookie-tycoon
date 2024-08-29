130368beb6f7f23cdbf6c720d92752b8c60056ba
Jacob DeBenedetto <jacobdebenedetto@ctlmvillalon.ur.com>
2024-08-29 10:57:30 -0500

    Add popup template

---
+++ src/vue/DataPopup.vue
@@ -0,0 +1,108 @@
+<script setup>
+  import { ref, onMounted, onUnmounted } from 'vue';
+
+  // Initialize attributes
+  var text = ref('');
+  var inputs = ref([]);
+  var isOpen = ref(false);
+
+  // Add event listener(s)
+  function addEventListeners() {
+    window.addEventListener('openDataPopup', openDataPopup);
+    window.addEventListener('closeDataPopup', closeDataPopup);
+    window.addEventListener('keydown', keydown);
+  }
+  
+  // Remove event listeners
+  function removeEventListeners() {
+    window.removeEventListener('openDataPopup', openDataPopup);
+    window.removeEventListener('closeDataPopup', closeDataPopup);
+    window.removeEventListener('keydown', keydown);
+  }
+
+  function openDataPopup(e) {
+    isOpen.value = true;
+
+    // Assign values from custom event detail
+    if (e.detail) {
+      if (e.detail.text) text.value = e.detail.text;
+      if (e.detail.inputs) {
+        inputs.value = e.detail.inputs;
+        inputs.value.forEach(function(input) {
+          // Set input event type (ex: click vs change)
+          if (input.type == 'file' || input.type == 'range' || input.type == 'text') input.event = 'change';
+          else input.event = 'click';
+        })
+      }
+    }
+
+    // Trigger opened event
+    setTimeout(function() {
+      window.dispatchEvent(new CustomEvent('popupOpened'));
+    }, 100);
+  }
+
+  function closeDataPopup() {
+    isOpen.value = false;
+
+    // Trigger opened event
+    setTimeout(function() {
+      window.dispatchEvent(new CustomEvent('popupClosed'));
+    }, 100);
+  }
+
+  function runCallback(callback, e) {
+    if (callback == null) callback = closeDataPopup;
+    callback(e);
+  }
+
+  function runLastInputCallback(e) {
+    var lastInput = inputs.value[inputs.value.length - 1];
+    if (lastInput) runCallback(lastInput.callback, e);
+  }
+
+  function keydown(e) {
+    if (isOpen.value == true) {
+      var jumpKeys = ['Space', 'Enter', 'Escape'];
+      if (jumpKeys.indexOf(e.code) > -1) {
+        // Close popup
+        e.preventDefault();
+        runLastInputCallback(e);
+      }
+    }
+  }
+
+  onMounted(function() {
+    addEventListeners();
+  });
+
+  onUnmounted(function() {
+    removeEventListeners();
+  });
+</script>
+
+<template>
+  <Transition name="fade-bubble-popup">
+    <div class="popup" v-if="isOpen == true">
+      <div class="background" @click="runLastInputCallback"></div>
+      <div class="container">
+        <div class="content">
+          <p class="text" v-html="text"></p>
+          <div class="inputs">
+            <template v-for="(input, index) of inputs">
+              <label v-if="input.label" :for="'popup-' + input.type + '-' + index">{{ input.label }}</label>
+              <input :class="input.class" :id="'popup-' + input.type + '-' + index" :type="input.type" :value="input.value" :min="input.min" :max="input.max" :step="input.step" :accept="input.accept" :style="input.style" v-on:[input.event]="runCallback(input.callback, $event)">
+            </template>
+          </div>
+          <a class="close" @click="runLastInputCallback">
+            <span class="material-symbols-rounded">close</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+<style>
+  .fade-bubble-popup-enter-active, .fade-bubble-popup-leave-active { transition: opacity 0.1s ease; }
+  .fade-bubble-popup-enter-from, .fade-bubble-popup-leave-to { opacity: 0; }
+</style>
--- src/vue/Menu.vue
+++ src/vue/Menu.vue
@@ -1,57 +1,66 @@
 <script setup>
-	import { ref, onMounted } from 'vue';
-	import '../scss/menu.scss';
+  import { ref, onMounted } from 'vue';
+  import '../scss/menu.scss';
 
-	// Init refs
-	var isOpen = ref(false)
-	var props = defineProps(['game']);
-	var version = ref('0.0.0');
+  // Init refs
+  var isOpen = ref(false)
+  var props = defineProps(['game']);
+  var version = ref('0.0.0');
 
-	async function updateVersion() {
-		// Load version asynchronously (Vite doesn't like JSON from /public dir)
-        var response = await fetch('/manifest.json');
-        var json = await response.json();
-        version.value = json.version;
-	}
+  async function updateVersion() {
+    // Load version asynchronously (Vite doesn't like JSON from /public dir)
+    var response = await fetch('/manifest.json');
+    var json = await response.json();
+    version.value = json.version;
+  }
 
-	function toggleMenu() {
-		isOpen.value = !isOpen.value;
-	}
+  function toggleMenu() {
+    isOpen.value = !isOpen.value;
+  }
 
-	function openFullscreen() {
-		if (chrome.tabs) chrome.tabs.create({ url: location.href });
-		else window.open(location.href, '_blank');
-    }
-	
-	onMounted(async () => {
-		updateVersion();
-	});
+  function openFullscreen() {
+    if (chrome.tabs) chrome.tabs.create({ url: location.href });
+    else window.open(location.href, '_blank');
+  }
+
+  function syncData() {
+    dispatchEvent(new CustomEvent('openDataPopup'), { detail: data });
+  }
+  
+  onMounted(async () => {
+    updateVersion();
+  });
 </script>
 
 <template>
-	<div class="menu">
-		<button class="toggle" :class="{ open: isOpen == true }" @click="toggleMenu()">
-			<label class="color">
-				<span class="material-symbols-outlined">settings</span>
-				<span class="version">v{{ version }}</span>
-				<span class="close"><span class="material-symbols-outlined">close</span></span>
-			</label>
-		</button>
-		<ul class="nav" :class="{ visible: isOpen == true }">
-			<li>
-				<button @click="openFullscreen()">
-					<span class="material-symbols-outlined">fullscreen</span> Open Fullscreen
-				</button>
-			</li>
-			<li class="stats">
-				<div class="label"><span class="material-symbols-outlined">bar_chart</span> Cookie Production</div>
-				<ul>
-					<li v-for="(item, key, index) of game.shop" :style="{ animationDelay: (index * 50) + 'ms' }">
-						<span class="icon material-symbols-outlined" :style="{ 'background': item.icon.color }" v-if="item.icon">{{ item.icon.name }}</span>
-						<span class="text">{{ item.name }}: +{{ game.getItemAmount(key).toLocaleString() }}{{ item.tooltip }}</span>
-					</li>
-				</ul>
-			</li>
-		</ul>
-	</div>
+  <div class="menu">
+    <button class="toggle" :class="{ open: isOpen == true }" @click="toggleMenu()">
+      <label class="color">
+        <span class="material-symbols-outlined">settings</span>
+        <span class="version">v{{ version }}</span>
+        <span class="close"><span class="material-symbols-outlined">close</span></span>
+      </label>
+    </button>
+    <ul class="nav" :class="{ visible: isOpen == true }">
+      <li>
+        <button @click="openFullscreen()">
+          <span class="material-symbols-outlined">fullscreen</span> Open Fullscreen
+        </button>
+      </li>
+      <li>
+        <button @click="syncData()">
+          <span class="material-symbols-outlined">cloud_upload</span> Sync Data
+        </button>
+      </li>
+      <li class="stats">
+        <div class="label"><span class="material-symbols-outlined">bar_chart</span> Cookie Production</div>
+        <ul>
+          <li v-for="(item, key, index) of game.shop" :style="{ animationDelay: (index * 50) + 'ms' }">
+            <span class="icon material-symbols-outlined" :style="{ 'background': item.icon.color }" v-if="item.icon">{{ item.icon.name }}</span>
+            <span class="text">{{ item.name }}: +{{ game.getItemAmount(key).toLocaleString() }}{{ item.tooltip }}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
